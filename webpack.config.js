245a8681ddf3780355f7157071d0bbdf4eb08959
Pritimay Sarkar <pritimay.sarkar@rupeek.com>
2019-08-23 15:26:14 +0530

    webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,34 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-module.exports = {
-    entry: {
-        app: "./src/index.js",
-    },
-    output: {
-        path: __dirname + "/dist",
-        filename: "[name].js",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack demo",
-        }),
-    ],
-    devServer: {
-        // Display only errors to reduce the amount of output.
-        stats: "errors-only",
+const parts = require("./webpack.parts");
+const merge = require("webpack-merge");
 
-        // Parse host and port from env to allow customization.
-        //
-        // If you use Docker, Vagrant or Cloud9, set
-        // host: "0.0.0.0";
-        //
-        // 0.0.0.0 is available to all network devices
-        // unlike default `localhost`.
-        host: process.env.HOST, // Defaults to `localhost`
-        port: process.env.PORT, // Defaults to 8080
-        open: false, // Open the page in browser
-        overlay: true,
-        watchOptions: {
-            // Delay the rebuild after the first change
-            aggregateTimeout: 300,
-
-            // Poll using interval (in ms, accepts boolean too)
-            poll: 1000,
-        },
+const commonConfig = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack demo",
+            }),
+        ],
     },
-}
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+]);
+
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, { mode });
+    }
+
+    return merge(commonConfig, developmentConfig, { mode });
+};
